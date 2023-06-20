a350a67e75b4b6b5a90489ac636fb8665bb42b5a
princeallan <allankirui57@gmail.com>
2023-06-20 17:12:22 +0300

    frontend blade added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,185 @@
-@extends('layouts.base')
+{{--@extends('layouts.base')--}}
 
-@section('body')
-    @yield('content')
-    
-    @isset($slot)
-        {{ $slot }}
-    @endisset
-@endsection
+{{--@section('body')--}}
+{{--    @yield('content')--}}
+{{--    --}}
+{{--    @isset($slot)--}}
+{{--        {{ $slot }}--}}
+{{--    @endisset--}}
+{{--@endsection--}}
+    <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Property Manager</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+
+    <!-- Tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+
+        .font-family-karla {
+            font-family: karla;
+        }
+    </style>
+
+    <!-- AlpineJS -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <!-- Font Awesome -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+            integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+</head>
+<body class="bg-white font-family-karla">
+
+<!-- Top Bar Nav -->
+<nav class="w-full py-4 bg-blue-800 shadow">
+    <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
+
+        <nav>
+            <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+                <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Buy</a></li>
+                <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Rent</a></li>
+            </ul>
+        </nav>
+
+        <div class="flex items-center text-lg no-underline text-white pr-6">
+            <a class="" href="#">
+                <i class="fab fa-facebook"></i>
+            </a>
+            <a class="pl-6" href="#">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a class="pl-6" href="#">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a class="pl-6" href="#">
+                <i class="fab fa-linkedin"></i>
+            </a>
+
+            @if (Route::has('login'))
+                @auth
+                    <a href="{{ url('admin') }}"
+                       class="font-semibold pl-6 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}"
+                       class="font-semibold pl-6 hover:text-white-900 dark:text-white-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Log
+                        in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                           class="ml-4 font-semibold pl-6 text-white-600 hover:text-white-900 dark:text-white-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Register</a>
+                    @endif
+                @endauth
+            @endif
+        </div>
+    </div>
+
+</nav>
+
+<!-- Text Header -->
+<header class="w-full mx-auto bg-gray-300">
+    <div class="flex flex-col items-center py-6">
+        <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
+            My Property Site
+        </a>
+        <p class="text-lg text-gray-600">
+            Quality Homes
+        </p>
+    </div>
+</header>
+
+<div class="container mx-auto flex flex-wrap py-6">
+
+        {{$slot}}
+
+
+    <!-- Sidebar Section -->
+    <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+
+        <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+            <p class="text-xl font-semibold pb-5">About Us</p>
+            <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio
+                sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+            <a href="#"
+               class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                Get to know us
+            </a>
+        </div>
+
+        <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+            <p class="text-xl font-semibold pb-5">Instagram</p>
+            <div class="grid grid-cols-3 gap-3">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
+               </div>
+            <a href="#"
+               class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
+                <i class="fab fa-instagram mr-2"></i> Follow @myproperties
+            </a>
+        </div>
+
+    </aside>
+
+</div>
+
+<footer class="w-full border-t bg-white pb-12">
+    <div
+        class="relative w-full flex items-center invisible md:visible md:pb-12"
+        x-data="getCarouselData()"
+    >
+        <button
+            class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+            x-on:click="decrement()">
+            &#8592;
+        </button>
+        <template x-for="image in images.slice(currentIndex, currentIndex + 6)" :key="images.indexOf(image)">
+            <img class="w-1/6 hover:opacity-75" :src="image">
+        </template>
+        <button
+            class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+            x-on:click="increment()">
+            &#8594;
+        </button>
+    </div>
+    <div class="w-full container mx-auto flex flex-col items-center">
+        <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+            <a href="#" class="uppercase px-3">About Us</a>
+            <a href="#" class="uppercase px-3">Privacy Policy</a>
+            <a href="#" class="uppercase px-3">Terms & Conditions</a>
+            <a href="#" class="uppercase px-3">Contact Us</a>
+        </div>
+        <div class="uppercase pb-6">&copy; mysite.com</div>
+    </div>
+</footer>
+
+<script>
+    function getCarouselData() {
+        return {
+            currentIndex: 0,
+            images: [
+                'https://source.unsplash.com/collection/1346951/800x800?sig=1',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=2',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=3',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=4',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=5',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=6',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=7',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=8',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=9',
+            ],
+            increment() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
+            },
+            decrement() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
+            },
+        }
+    }
+</script>
+
+</body>
+</html>
