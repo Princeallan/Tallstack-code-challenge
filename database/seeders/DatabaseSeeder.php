ea679580f35bd9d78090ac80e3cc5a0f2dd4990c
princeallan <allankirui57@gmail.com>
2023-06-22 20:30:31 +0300

    added the user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\PropertyStatus;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//         \App\Models\Property::factory(50)->create();
+         \App\Models\Property::factory(50)->create();
         $this->call([
-//            PropertyTypeSeeder::class,
-//            PropertyStatusSeeder::class,
-//            CategorySeeder::class
-//            LocationSeeder::class
+            PropertyTypeSeeder::class,
+            PropertyStatusSeeder::class,
+            CategorySeeder::class,
+            LocationSeeder::class
+        ]);
+
+        User::factory()->create([
+            'email' => 'demoadmin@example.com',
+            'name' => 'Demo Admin',
+            'password' => bcrypt('Secret123!'),
+            'email_verified_at' => Carbon::now()
         ]);
     }
 }
